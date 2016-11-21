1ba772f6af8f56b4ccee3cdafe65fe661291e154
ucuceagit <ucutech50@gmail.com>
2016-11-21 22:54:45 +0800

    Created with Visual Studio 2015
    C# Demo program of E-Gizmo TF-MD-M1 Biometric Finger Print Scanner.
    Requires .net Framework 4.5

---
+++ Fingerprint Scanner Demo/frmDemo.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Fingerprint_Scanner_Demo
+{
+    partial class frmDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.uartRx = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtReply = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnIdentify = new System.Windows.Forms.Button();
+            this.btnEnroll = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txtTemplate = new System.Windows.Forms.TextBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnRemoveAll = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Serial Port:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(76, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(149, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(231, 24);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(112, 22);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // uartRx
+            // 
+            this.uartRx.Interval = 1;
+            this.uartRx.Tick += new System.EventHandler(this.uartRx_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtReply);
+            this.groupBox1.Location = new System.Drawing.Point(15, 268);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(602, 89);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Reply";
+            // 
+            // txtReply
+            // 
+            this.txtReply.Location = new System.Drawing.Point(21, 36);
+            this.txtReply.Name = "txtReply";
+            this.txtReply.Size = new System.Drawing.Size(564, 20);
+            this.txtReply.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnRemoveAll);
+            this.groupBox2.Controls.Add(this.btnRemove);
+            this.groupBox2.Controls.Add(this.txtTemplate);
+            this.groupBox2.Controls.Add(this.lblStatus);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.btnEnroll);
+            this.groupBox2.Controls.Add(this.btnIdentify);
+            this.groupBox2.Location = new System.Drawing.Point(15, 66);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(602, 196);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Commands";
+            // 
+            // btnIdentify
+            // 
+            this.btnIdentify.Location = new System.Drawing.Point(55, 89);
+            this.btnIdentify.Name = "btnIdentify";
+            this.btnIdentify.Size = new System.Drawing.Size(101, 23);
+            this.btnIdentify.TabIndex = 0;
+            this.btnIdentify.Text = "Identify";
+            this.btnIdentify.UseVisualStyleBackColor = true;
+            this.btnIdentify.Click += new System.EventHandler(this.btnIdentify_Click);
+            // 
+            // btnEnroll
+            // 
+            this.btnEnroll.Location = new System.Drawing.Point(181, 89);
+            this.btnEnroll.Name = "btnEnroll";
+            this.btnEnroll.Size = new System.Drawing.Size(101, 23);
+            this.btnEnroll.TabIndex = 1;
+            this.btnEnroll.Text = "Enroll";
+            this.btnEnroll.UseVisualStyleBackColor = true;
+            this.btnEnroll.Click += new System.EventHandler(this.btnEnroll_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(174, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Template No.:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(16, 139);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(568, 38);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Disconnected";
+            // 
+            // txtTemplate
+            // 
+            this.txtTemplate.Location = new System.Drawing.Point(254, 41);
+            this.txtTemplate.Name = "txtTemplate";
+            this.txtTemplate.Size = new System.Drawing.Size(103, 20);
+            this.txtTemplate.TabIndex = 4;
+            this.txtTemplate.Text = "1";
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(297, 89);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(101, 23);
+            this.btnRemove.TabIndex = 5;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnRemoveAll
+            // 
+            this.btnRemoveAll.Location = new System.Drawing.Point(416, 89);
+            this.btnRemoveAll.Name = "btnRemoveAll";
+            this.btnRemoveAll.Size = new System.Drawing.Size(101, 23);
+            this.btnRemoveAll.TabIndex = 6;
+            this.btnRemoveAll.Text = "Remove All";
+            this.btnRemoveAll.UseVisualStyleBackColor = true;
+            this.btnRemoveAll.Click += new System.EventHandler(this.btnRemoveAll_Click);
+            // 
+            // frmDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 378);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDemo";
+            this.Text = "Fingerprint Scanner Demo";
+            this.Load += new System.EventHandler(this.frmDemo_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer uartRx;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtReply;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEnroll;
+        private System.Windows.Forms.Button btnIdentify;
+        private System.Windows.Forms.Button btnRemoveAll;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.TextBox txtTemplate;
+    }
+}
+
---
+++ Fingerprint Scanner Demo/frmDemo.cs
@@ -0,0 +1,229 @@
+﻿/**
+ This code is released as is with no warranties
+ You are free to use and modify the code for personal use only
+
+ Written by: Richard Myrick T. Arellaga
+**/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Fingerprint_Scanner_Demo
+{
+    public partial class frmDemo : Form
+    {
+
+        byte[] fpsdata = new byte[24];
+        byte[] fpsreply = new byte[24];
+        int checksum;
+        int templateNumber;
+
+
+        public frmDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDemo_Load(object sender, EventArgs e)
+        {
+            foreach(string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if(btnConnect.Text == "Connect")
+            {
+                btnConnect.Text = "Disconnect";
+                try
+                {
+                    serialPort1.PortName = comboBox1.Text.ToString();
+                    serialPort1.BaudRate = 115200;
+                    serialPort1.Open();
+                    uartRx.Start();
+                    lblStatus.Text = "Connected to " + comboBox1.Text.ToString();
+                }
+                catch { }
+
+            }else if(btnConnect.Text == "Disconnect")
+            {
+                btnConnect.Text = "Connect";
+                try
+                {
+                    uartRx.Stop();
+                    serialPort1.DiscardInBuffer();
+                    serialPort1.DiscardOutBuffer();
+                    serialPort1.Open();
+                    lblStatus.Text = "Disconnected";
+                }
+                catch { }
+
+            }
+        }
+
+        private void calculateChecksum()
+        {
+            int x;
+            checksum = 0;
+            for (x = 0; x <= 21; x++)
+            {
+                checksum += fpsdata[x];
+            }
+
+            fpsdata[22] = Convert.ToByte(checksum & 0x00FF);
+            fpsdata[23] = Convert.ToByte((checksum & 0xFF00) >> 8); ;
+        }
+
+        private string byteToHex(byte[] combyte)
+        {
+            StringBuilder builder = new StringBuilder(combyte.Length);
+            foreach (byte data in combyte)
+                builder.Append(Convert.ToString(data, 16).PadLeft(2, '0').PadRight(3, ' '));
+            return builder.ToString().ToUpper();
+        }
+
+        private void uartRx_Tick(object sender, EventArgs e)
+        {
+            if (serialPort1.BytesToRead > 10)
+            {
+                serialPort1.Read(fpsreply, 0, 24);
+            }
+
+            txtReply.Text = byteToHex(fpsreply);
+
+            if (fpsreply[2] == 0x02 && fpsreply[3] == 0x01 && fpsreply[6] == 0x00 && fpsreply[8] != 0xF4)
+            {
+
+                lblStatus.Text = "Hello User " + fpsreply[8].ToString();
+
+            }
+            else if (fpsreply[2] == 0x02 && fpsreply[3] == 0x01 && fpsreply[6] == 0x01 && fpsreply[9] == 0x12 && fpsreply[8] != 0xF4)
+            {
+                lblStatus.Text = "You are not enrolled";
+            }
+            else if (fpsreply[2] == 0x03 && fpsreply[3] == 0x01 && fpsreply[8] == 0xF1 && fpsreply[8] != 0xF4)
+            {
+                lblStatus.Text = "Please press a finger to enroll";
+            }
+            else if (fpsreply[2] == 0x03 && fpsreply[3] == 0x01 && fpsreply[8] == 0xF2 && fpsreply[8] != 0xF4)
+            {
+                lblStatus.Text = "Please press the same finger second time...";
+            }
+            else if (fpsreply[2] == 0x03 && fpsreply[3] == 0x01 && fpsreply[8] == 0xF3 && fpsreply[8] != 0xF4)
+            {
+                lblStatus.Text = "Please press the same finger last time";
+            }
+            else if (fpsreply[2] == 0x03 && fpsreply[3] == 0x01 && fpsreply[8] == 0xF4 && fpsreply[8] != 0xF4)
+            {
+                lblStatus.Text = "Lift Finger";
+            }
+            else if (fpsreply[2] == 0x03 && fpsreply[3] == 0x01 && fpsreply[4] == 0x06 && fpsreply[8] != 0xF4)
+            {
+                lblStatus.Text = "Enrolled!";
+            }
+            else if (fpsreply[2] == 0x05 && fpsreply[3] == 0x01 && fpsreply[4] == 0x04 && fpsreply[8] != 0xF4)
+            {
+                lblStatus.Text = "User " + fpsreply[8].ToString() + " Deleted";
+            }
+            else if (fpsreply[2] == 0x06 && fpsreply[3] == 0x01 && fpsreply[6] == 0x00 && fpsreply[8] != 0xF4)
+            {
+                lblStatus.Text = fpsreply[8].ToString() + "Users Deleted!";
+            }else if(fpsreply[6] == 0x01 && fpsreply[8] == 0x15)
+            {
+                lblStatus.Text = "No enrolled fingerprint templates!";
+            }
+            else if (fpsreply[6] == 0x01 && fpsreply[8] == 0x14)
+            {
+                lblStatus.Text = "Location not empty";
+            }
+            else if (fpsreply[6] == 0x01 && fpsreply[8] == 0x21)
+            {
+                lblStatus.Text = "Bad image quality!";
+            }
+            else if (fpsreply[6] == 0x01 && fpsreply[8] == 0x60)
+            {
+                lblStatus.Text = "Location not valid";
+            }
+        }
+
+        private void btnIdentify_Click(object sender, EventArgs e)
+        {
+            Array.Clear(fpsdata, 0, 24);
+
+            fpsdata[0] = 0x55;
+            fpsdata[1] = 0xAA;
+            fpsdata[2] = 0x02;
+            fpsdata[3] = 0x01;
+
+            calculateChecksum();
+
+            serialPort1.Write(fpsdata, 0, 24);
+        }
+
+        private void btnEnroll_Click(object sender, EventArgs e)
+        {
+            Array.Clear(fpsdata, 0, 24);
+
+            templateNumber = Convert.ToInt32(txtTemplate.Text);
+
+            fpsdata[0] = 0x55;
+            fpsdata[1] = 0xAA;
+            fpsdata[2] = 0x03;
+            fpsdata[3] = 0x01;
+            fpsdata[4] = 0x02;
+            fpsdata[5] = Convert.ToByte((templateNumber & 0xFF00) >> 8);
+            fpsdata[6] = Convert.ToByte(templateNumber & 0x00FF);
+
+            calculateChecksum();
+
+            serialPort1.Write(fpsdata, 0, 24);
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            Array.Clear(fpsdata, 0, 24);
+
+            templateNumber = Convert.ToInt32(txtTemplate.Text);
+
+            fpsdata[0] = 0x55;
+            fpsdata[1] = 0xAA;
+            fpsdata[2] = 0x05;
+            fpsdata[3] = 0x01;
+            fpsdata[4] = 0x02;
+            fpsdata[5] = Convert.ToByte((templateNumber & 0xFF00) >> 8);
+            fpsdata[6] = Convert.ToByte(templateNumber & 0x00FF);
+
+            calculateChecksum();
+
+            serialPort1.Write(fpsdata, 0, 24);
+        }
+
+        private void btnRemoveAll_Click(object sender, EventArgs e)
+        {
+
+            Array.Clear(fpsdata, 0, 24);
+
+            fpsdata[0] = 0x55;
+            fpsdata[1] = 0xAA;
+            fpsdata[2] = 0x06;
+            fpsdata[3] = 0x01;
+            
+
+            calculateChecksum();
+
+            serialPort1.Write(fpsdata, 0, 24);
+
+        }
+    }
+}
